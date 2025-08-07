1f405c84323a38e2b65e1fa9b71e879be7922a0f
professorDJKim <dkim121@depaul.edu>
2025-08-07 18:44:24 -0500

    db

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,9 @@
-# No models yet, placeholder for Django app
+from django.db import models
+
+class Todo(models.Model):
+	title = models.CharField(max_length=255)
+	completed = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
